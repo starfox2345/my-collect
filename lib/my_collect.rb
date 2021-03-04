9da6f52809f959e106fb0b7668792cf0c93a9fd6
yatsun <yatsunlam25@gmail.com>
2021-03-03 23:44:46 -0500

    commit tree ed41d9562fb43bbb28cfd87dff02cb936ab43ccb

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
 
+def my_collect(files=["ruby", "javascript", "python", "objective-c"])
+    i = 0
+    collection = []
+
+    while i < files.length
+        collection << yield(files[i])
+        i += 1
+    end
+    collection
+
+end
 
